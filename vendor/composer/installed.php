f93dbd9bb7c79f409267c7e33e6f1c03704f8d27
PyKej <patrik.pk13@gmail.com>
2024-04-16 23:00:48 +0200

    samko help s composerom

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '390cf29ffab71adba37d508d10a87661f79efde7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '390cf29ffab71adba37d508d10a87661f79efde7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'workerman/workerman' => array(
+            'pretty_version' => 'v4.1.15',
+            'version' => '4.1.15.0',
+            'reference' => 'afc8242fc769ab7cf22eb4ac22b97cb59d465e4e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../workerman/workerman',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
